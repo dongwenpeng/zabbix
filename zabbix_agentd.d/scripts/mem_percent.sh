102f6e91dce882e6141017b777a6635d43b367e3
dongwenpeng <18612100045@163.com>
2016-10-09 13:32:30 +0800

    fix memory count bug

--- zabbix_agentd.d/scripts/mem_percent.sh
+++ zabbix_agentd.d/scripts/mem_percent.sh
@@ -6,3 +6,4 @@ Cached=`grep -w Cached /proc/meminfo | awk '{print $2}'`
 Used=`free -k | grep -b1 used | awk '{print $3}' | tail -n1`
 MemUse=`echo $[$Used-$Buffers-$Cached]`
 python -c "print float(`echo $MemUse`)/float(`echo $MemTotal`)"
+
