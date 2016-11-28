5898674c6bbf2c5731d0d6f5e32b6cc8ea003a1e
彭东稳 <pengdongwen@ehousechina.com>
2016-11-28 16:43:59 +0800

    fix bug

--- zabbix_agentd.d/scripts/mem_percent.sh
+++ zabbix_agentd.d/scripts/mem_percent.sh
@@ -5,5 +5,5 @@ Buffers=`grep Buffers /proc/meminfo | awk '{print $2}'`
 Cached=`grep -w Cached /proc/meminfo | awk '{print $2}'`
 Used=`free -k | grep -b1 used | awk '{print $3}' | tail -n1`
 MemUse=`echo $[$Used-$Buffers-$Cached]`
-python -c "print float(`echo $MemUse`)/float(`echo $MemTotal`)"
+python -c "print float(`echo $MemUse`)/float(`echo $MemTotal`)" | cut -c 1,2,3,4
 
